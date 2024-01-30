0da72fc4ef21e423e1bd45a9c79ff7375527ff9d
vacool6 <sameerkittur321@gmail.com>
2024-01-30 11:21:37 +0530

    app without ls

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
-const items = localStorage.getItem("items");
+let items = [];
 
-if (!items) {
-  localStorage.setItem("items", JSON.stringify([]));
-} else {
-  for (let i of JSON.parse(items)) {
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let value = input.value;
+
+  if (!value) return;
+
+  items.push(value);
+
+  input.value = "";
+  ul.innerHTML = "";
+
+  for (let i of items) {
     const li = document.createElement("li");
     const p = document.createElement("p");
     const button = document.createElement("button");
@@ -16,32 +24,11 @@ if (!items) {
     li.append(p);
     li.append(button);
     ul.append(li);
-  }
-}
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let value = input.value;
-  const newItems = [...JSON.parse(items), value];
-  localStorage.setItem("items", JSON.stringify(newItems));
-  value = "";
-  window.location.reload();
-});
-
-if (document.querySelectorAll(".remove")) {
-  for (let i of document.querySelectorAll(".remove")) {
-    i.addEventListener("click", function () {
-      // Works without LS
-      // const listItem = this.parentNode;
-      // const ul = listItem.parentNode;
-      // ul.removeChild(listItem);
-
-      // With LS
-      const listItem = this.parentNode;
-      const value = listItem.innerText.split("\n")[0];
-      const updateItems = JSON.parse(items).filter((val) => val !== value);
-      localStorage.setItem("items", JSON.stringify(updateItems));
-      window.location.reload();
+    button.addEventListener("click", () => {
+      items = items.filter((i) => i !== value);
+      const listItem = li;
+      ul.removeChild(listItem);
     });
   }
-}
+});
